cd608c9ae91a0c6d5a7fc88de72823a3231931c1
Rud356 <r.moroshkin2015@gmail.com>
2024-05-28 17:10:42 +0300

    Add files that are missing in previous commit

---
+++ src/App.ProjectSettings/Events/MarkupClassDeleted.cs
@@ -0,0 +1,13 @@
+﻿using Prism.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.ProjectSettings.Events
+{
+    public class MarkupClassDeletedEvent : PubSubEvent<int>
+    {
+    }
+}
---
+++ src/App.ProjectSettings/Models/ObservableString.cs
@@ -0,0 +1,31 @@
+﻿// ObservableString.cs
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+public class ObservableString : INotifyPropertyChanged
+{
+    private string _value;
+    public string Value
+    {
+        get { return _value; }
+        set
+        {
+            if (_value != value)
+            {
+                _value = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public override string ToString()
+    {
+        return Value;
+    }
+}
